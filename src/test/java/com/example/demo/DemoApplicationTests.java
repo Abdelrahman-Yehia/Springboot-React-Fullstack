ec0752d67d0db4860d49ff996c63f049f93f7b42
Abdelrahman Yehia <akayehia@amazon.lu>
2023-05-15 17:14:33 +0200

    removed failing test and changed name in footer. All check should pass

--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -1,6 +1,5 @@
 package com.example.demo;
 
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -9,7 +8,6 @@ class DemoApplicationTests {
 
 	@Test
 	void contextLoads() {
-		Assertions.fail("Ops, test failed!");
 	}
 
 }
